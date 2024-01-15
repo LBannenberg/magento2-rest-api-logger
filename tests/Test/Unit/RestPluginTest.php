6fcfa5f943bd0c424dabf659b8c13b768928b3f3
lau <lau.bannenberg@gmail.com>
2024-01-15 18:42:10 +0100

    Add unit tests for FilterProcessor.php

---
+++ tests/Test/Unit/RestPluginTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Corrivate\RestApiLogger\Tests\Unit;
+
+use Corrivate\RestApiLogger\Plugin\Magento\Webapi\Controller\RestPlugin;
+use PHPUnit\Framework\TestCase;
+
+class RestPluginTest extends TestCase
+{
+
+}
